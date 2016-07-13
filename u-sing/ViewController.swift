79b914cc20dfb49a19768a46df7df229a3d7ad1e
Huy Truong <HT@Huys-MacBook-Pro.local>
2016-07-13 14:12:32 -0700

    everything works including inserting the overlay at any point. **Must delete the audio file in iTunes because it plays the previous one**

--- u-sing/ViewController.swift
+++ u-sing/ViewController.swift
@@ -13,21 +13,18 @@ import CoreMedia
 class ViewController: UIViewController {
     
     
-    var composition =  AVMutableComposition()
-    var originalTrack:AVMutableCompositionTrack!
-    var index = 0
-    //array to store all tracks
-    var allTracks: [AnyObject] = ["1", "2","1"]
-    var allTracks1: [AnyObject] = ["1", "2"]
-    let finalComposed = AVMutableComposition()
+//    var composition =  AVMutableComposition()
+//     var originalTrack:AVMutableCompositionTrack!
+//    var index = 0
+//    //array to store all tracks
+//    var allTracks: [AnyObject] = ["1", "2"]
+//    var allTracks1: [AnyObject] = ["1","2"]
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //setOriginalSong()
-        //originalTrack.play()
         mashingSongFilesAndPlayBoth()
         
         
@@ -35,53 +32,70 @@ class ViewController: UIViewController {
     }
     
     
-    //setting the background music
-    //    func setOriginalSong (){
-    //
-    //        let bgMusicUrl:NSURL = NSBundle.mainBundle().URLForResource("1", withExtension: "m4a")!
-    //
-    //        do {
-    //            originalTrack =  try AVAudioPlayer(contentsOfURL:bgMusicUrl)
-    //            originalTrack.prepareToPlay()
-    //
-    //            //setting the volume
-    //            originalTrack.volume = 0.3
-    //
-    //        } catch _ {
-    //            print("song is not found.")
-    //        }
-    //    }
-    
-    
+    ////////////////////////setting the background music//////////////////////
+//    func lowerPartOfSongVolume (asset: AVAssetExportSession){
+//    
+//            let bgMusicUrl:NSURL = NSBundle.mainBundle().URLForResource("1", withExtension: "m4a")!
+//    
+//            do {
+//                originalTrack =  try AVAudioPlayer(contentsOfURL:bgMusicUrl)
+//                originalTrack.prepareToPlay()
+//    
+//                //setting the volume
+//                originalTrack.volume = 0.3
+//                
+//                //playing the song
+//                originalTrack.play()
+//    
+//            } catch _ {
+//                print("song is not found.")
+//            }
+//        }
+  
     //////////////////////////////////////////////////////////////////////////////
-    func mashingSongFilesAndPlayBoth(){
+       func mashingSongFilesAndPlayBoth(){
         
-        
+        var allTracks1: [AnyObject] = ["1","2"]
+        var composition =  AVMutableComposition()
+        var originalTrack:AVMutableCompositionTrack!
+        //var originalTrack: AVMutableCompositionTrack!
+        var index = 0
+
         for trackName in allTracks1{
             
             let audioAsset: AVURLAsset = AVURLAsset(URL: NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource(trackName as? String, ofType: "m4a")!), options: nil)
             
-            var audioTrack: AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+            var audioTrack: AVMutableCompositionTrack!
             
-            if(index == 0){
-                print("im there")
+            if index == 0 {
+                originalTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+            }
+            else if index == 1{
+                audioTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+            }
+            
+            
+            //starting the merge process
+            if (index == 0) {
+                print("index = 0")
                 do {
-                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
-                }catch _ {
-                    print("shit, it didn't work")
+                    try originalTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
                 }
-            } else if(index > 0){
-                print("im here'")
-                
-                do {
-                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+12, audioAsset.duration.timescale))
-                }catch _ {
-                    print("shit, it didn't work")
+                catch _ {
+                    print("didn't wor")
                 }
             }
-            index = index + 1
+            else if (index == 1) {
+                print("index > 0")
+                do {
+                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+50, audioAsset.duration.timescale))
+                }
+                catch _ {
+                    print("didn't wor")
+                }
+            }
+           index = index + 1
         }
-        
         let assetExport: AVAssetExportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)!
         let mixedAudio: String = "mixedAudio.m4a"
         let exportPath: String = NSTemporaryDirectory().stringByAppendingString(mixedAudio)
@@ -90,8 +104,7 @@ class ViewController: UIViewController {
             do {
                 try NSFileManager.defaultManager().removeItemAtPath(exportPath)
             }
-            catch let error {
-                print(error)
+            catch _ {
             }
         }
         assetExport.outputFileType = AVFileTypeAppleM4A
@@ -100,77 +113,137 @@ class ViewController: UIViewController {
         assetExport.exportAsynchronouslyWithCompletionHandler({() -> Void in
             print("Completed Sucessfully")
         })
-    }
+
+    
+}
+
+    //////////////////////////////////////////////////////////////////////////////
+//    func mashingSongFilesAndPlayBoth(){
+//        
+//        
+//        
+//    
+//        
+//        for trackName in allTracks1{
+//            
+//            let audioAsset: AVURLAsset = AVURLAsset(URL: NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource(trackName as? String, ofType: "m4a")!), options: nil)
+//            
+//            
+//            
+//            if(index == 0){
+//                print("im there")
+//                do {
+//                    let audioTrack: AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//                    //try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds((CMTimeGetSeconds(kCMTimeZero)+40), 1))//audioAsset.duration.timescale))
+//                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds((CMTimeGetSeconds(kCMTimeZero)+20), audioAsset.duration.timescale))
+//                    
+//                }catch _ {
+//                    print("shit, it didn't work")
+//                }
+//            } else if(index > 0){
+//                print("im here'")
+//                
+//                do {
+//                    let originalTrack:AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//                    try originalTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
+//                }catch _ {
+//                    print("shit, it didn't work")
+//                }
+//            }
+//            index = index + 1
+//        }
+//        
+//        
+//        let assetExport: AVAssetExportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)!
+//        let mixedAudio: String = "mixedAudio.m4a"
+//        let exportPath: String = NSTemporaryDirectory().stringByAppendingString(mixedAudio)
+//        let exportURL: NSURL = NSURL.fileURLWithPath(exportPath)
+//        if NSFileManager.defaultManager().fileExistsAtPath(exportPath) {
+//            do {
+//                try NSFileManager.defaultManager().removeItemAtPath(exportPath)
+//            }
+//            catch let error {
+//                print(error)
+//            }
+//        }
+//        assetExport.outputFileType = AVFileTypeAppleM4A
+//        assetExport.outputURL = exportURL
+//        assetExport.shouldOptimizeForNetworkUse = true
+//        assetExport.exportAsynchronouslyWithCompletionHandler({() -> Void in
+//            print("Completed Sucessfully")
+//        })
+//        
+//        
+//
+//        
+//    }
 
     
     
-    ////////////////////////mashing all music files together/////////////////////////////
-    func mashingSongFilesAndCutitOff(){
-        
-        var audioTrack :AVMutableCompositionTrack!
-        
-        for trackName in allTracks{
-            let audioAsset: AVURLAsset = AVURLAsset(URL: NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource(trackName as? String, ofType: "m4a")!), options: nil)
-            //var audioTrack: AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
-            
-            //            originalTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
-            
-            if (index == 0) {
-                
-                originalTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
-                
-            } else if (index == 1) {
-                audioTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
-            }
-            //
-            
-            
-            
-            if(index == 0){
-                print("im there")
-                do {
-                    try originalTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+12, audioAsset.duration.timescale)), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
-                }catch _ {
-                    print("shit, it didn't work")
-                }
-            }
-            else if(index == 1){
-                print("im here'")
-                
-                do {
-                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+12, audioAsset.duration.timescale))
-                }catch _ {
-                    print("shit, it didn't work")
-                }
-            } else {
-                do {
-                    try originalTrack.insertTimeRange(CMTimeRangeMake(CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+12, audioAsset.duration.timescale), audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+22, audioAsset.duration.timescale))
-                }catch _ {
-                    print("shit, it didn't work")
-                }
-            }
-            index = index + 1
-        }
-        
-        let assetExport: AVAssetExportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)!
-        let mixedAudio: String = "mixedAudio.m4a"
-        let exportPath: String = NSTemporaryDirectory().stringByAppendingString(mixedAudio)
-        let exportURL: NSURL = NSURL.fileURLWithPath(exportPath)
-        if NSFileManager.defaultManager().fileExistsAtPath(exportPath) {
-            do {
-                try NSFileManager.defaultManager().removeItemAtPath(exportPath)
-            }
-            catch let error {
-                print(error)
-            }
-        }
-        assetExport.outputFileType = AVFileTypeAppleM4A
-        assetExport.outputURL = exportURL
-        assetExport.shouldOptimizeForNetworkUse = true
-        assetExport.exportAsynchronouslyWithCompletionHandler({() -> Void in
-            print("Completed Sucessfully")
-        })
-    }
+//    ////////////////////////mashing all music files together/////////////////////////////
+//    func mashingSongFilesAndCutitOff(){
+//        
+//        var audioTrack :AVMutableCompositionTrack!
+//        var originalTrack : AVMutableCompositionTrack!
+//        for trackName in allTracks{
+//            let audioAsset: AVURLAsset = AVURLAsset(URL: NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource(trackName as? String, ofType: "m4a")!), options: nil)
+//            //var audioTrack: AVMutableCompositionTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//            
+//            //            originalTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//            
+//            if (index == 0) {
+//                
+//                originalTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//                
+//            } else if (index == 1) {
+//                audioTrack = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+//            }
+//            //
+//            
+//            
+//            
+//            if(index == 0){
+//                print("im there")
+//                do {
+//                    try originalTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
+//                }catch _ {
+//                    print("shit, it didn't work")
+//                }
+//            }
+//            else if(index == 1){
+//                print("im here'")
+//                
+//                do {
+//                    try audioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioAsset.duration), ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: CMTimeMakeWithSeconds(CMTimeGetSeconds(kCMTimeZero)+80, audioAsset.duration.timescale))
+//                }catch _ {
+//                    print("shit, it didn't work")
+//                }
+//            }
+//            index = index + 1
+//        }
+//        
+//        print("before composition")
+//        print(composition)
+//        
+//        let assetExport: AVAssetExportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)!
+//        let mixedAudio: String = "mixedAudio.m4a"
+//        let exportPath: String = NSTemporaryDirectory().stringByAppendingString(mixedAudio)
+//        let exportURL: NSURL = NSURL.fileURLWithPath(exportPath)
+//        if NSFileManager.defaultManager().fileExistsAtPath(exportPath) {
+//            do {
+//                try NSFileManager.defaultManager().removeItemAtPath(exportPath)
+//            }
+//            catch let error {
+//                print(error)
+//            }
+//        }
+//        assetExport.outputFileType = AVFileTypeAppleM4A
+//        assetExport.outputURL = exportURL
+//        assetExport.shouldOptimizeForNetworkUse = true
+//        assetExport.exportAsynchronouslyWithCompletionHandler({() -> Void in
+//            print("Completed Sucessfully")
+//        })
+//    }
     
     
     
